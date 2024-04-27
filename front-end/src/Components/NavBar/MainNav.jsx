6e70e7b4c192f051b91258479c7390bd15ed3524
Abdelrahman Abdelnasser Gamal <2021170298@cis.asu.edu.eg>
2024-04-27 13:50:09 +0300

    Update MainNav.jsx

--- front-end/src/Components/NavBar/MainNav.jsx
+++ front-end/src/Components/NavBar/MainNav.jsx
@@ -1,52 +1,52 @@
 import React from 'react'
-import  logo from "../../assets/images/logo/logo.png"
+import logo from "../../assets/images/logo/logo.png"
 import "../../assets/styles/style.css"
 import { Link } from 'react-router-dom'
 
 function MainNav() {
-  return (
-<div className="themesflat-container">
-                    <div className="row">
-                    <div className="col-md-12">                              
-                    <div id="site-header-inner"> 
+    return (
+        <div className="themesflat-container">
+            <div className="row">
+                <div className="col-md-12">
+                    <div id="site-header-inner">
                         <div className="flex items-center justify-between ">
                             <div id="site-logo">
-                            <div id="site-logo-inner">
-                                <Link to="/" rel="home" className="main-logo">
-                                <img id="logo_header"  className="w-25 " src={logo} />
-                                </Link>
+                                <div id="site-logo-inner">
+                                    <Link to="/" rel="home" className="main-logo">
+                                        <img id="logo_header" className="w-25 " src={logo} />
+                                    </Link>
+                                </div>
                             </div>
-                            </div>
-                  
+
 
                             <nav id="main-nav" class="main-nav">
-                                        <ul id="menu-primary-menu" class="menu">
-                                            <li class="menu-item menu-item-has-children">
-                                                <Link to='/'>Home</Link>
-                                            </li>
-                                            <li class="menu-item">
-                                                <Link to="/404">404</Link>
-                                            </li>
-                                            <li class="menu-item menu-item-has-children">
-                                                <Link to='/maintenance'>Maintenance</Link>
-                                               
-                                            </li>
-                                            <li class="menu-item ">
-                                                <Link to="/contact">Contact</Link>
-                                            </li>
-                                        </ul>
-                                    </nav>
-                                    
-                        
-                                        <div class="canvas">
-                                            <span></span>
-                                        </div>
-                        </div> 
+                                <ul id="menu-primary-menu" class="menu">
+                                    <li class="menu-item ">
+                                        <Link to='/'>Home</Link>
+                                    </li>
+                                    <li class="menu-item">
+                                        <Link to="/404">404</Link>
+                                    </li>
+                                    <li class="menu-item ">
+                                        <Link to='/maintenance'>Maintenance</Link>
+
+                                    </li>
+                                    <li class="menu-item ">
+                                        <Link to="/contact">Contact</Link>
+                                    </li>
+                                </ul>
+                            </nav>
+
+
+                            <div class="canvas">
+                                <span></span>
+                            </div>
                         </div>
                     </div>
-                    </div>
                 </div>
-  )
+            </div>
+        </div>
+    )
 }
 
 export default MainNav
