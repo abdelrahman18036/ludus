d0a3a98b90eef0a560b2efacc32b4736a38b33a6
bahrawyX <96246479+bahrawyX@users.noreply.github.com>
2024-04-27 15:32:20 +0300

    tets

--- front-end/src/Components/Market/Components/MarketContent.jsx
+++ front-end/src/Components/Market/Components/MarketContent.jsx
@@ -6,7 +6,7 @@ import { baseURL } from "../../Auth/API";
 import axios from "axios";
 import profilePic from "../../../assets/images/avatar/avatar-01.png";
 import { Link } from "react-router-dom";
-
+import { motion } from 'framer-motion';
 
 export default function MarketContent() {
     const [nfts, setNfts] = useState([]);
@@ -54,130 +54,153 @@ export default function MarketContent() {
         }
     };
 
+const containerVariants = {
+    hidden: { opacity: 0, transform: 'translateY(100px)' },
+    visible: { 
+        opacity: 1,
+        transform: 'translateY(0px)',
+        transition: { duration: 0.5 }
+    }
+};
 
-    return (
-        <div id="market" className="tabcontent active">
-            <div className="wrapper-content">
-                <div className="inner-content">
 
-                    <div className="heading-section">
-                        <h2 className="tf-title pb-30">Trending NFTs</h2>
-                    </div>
-                    <div className="widget-tabs relative mb-40">
+const itemVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { 
+        opacity: 1,
+        y: 0,
+        transition: { duration: 0.8 }
+    }
+};
 
-                        <div className="widget-content-tab">
-                            <div className="widget-content-inner">
-                                <div className="featured-item style-bottom">
-                                    <Swiper
-                                        modules={[Navigation, Pagination]}
-                                        spaceBetween={26.7}
-                                        slidesPerView={1}
-                                        navigation
-                                        pagination={{ clickable: true }}
-                                        observer={true}
-                                        observeParents={true}
-                                        breakpoints={{
-                                            768: { slidesPerView: 2 },
-                                            1300: { slidesPerView: 3 },
-                                            1500: { slidesPerView: 4 }
-                                        }}
-                                        className="featured pt-10 swiper-container carousel1"
-                                    >
-                                        {nfts.map(nft => (
-                                            <SwiperSlide className="relative z-99999" key={nft._id}>
-                                                <Link to={`/nft/${nft._id}`}>
-                                                    <div className="tf-card-box style-4">
-                                                        <div className="author flex items-center">
-                                                            <div className="avatar">
-                                                                <img src={nft.author.profilePicture || profilePic} alt={nft.author.username} />
-                                                            </div>
-                                                            <div className="info">
-                                                                <span>Created by:</span>
-                                                                <h6><a href={`author-2.html`}>{nft.author.username}</a></h6>
-                                                            </div>
+return (
+    <div id="market" className="tabcontent active">
+        <div className="wrapper-content">
+            <div className="inner-content">
+                <div className="heading-section">
+                    <h2 className="tf-title pb-30">Trending NFTs</h2>
+                </div>
+                <div className="widget-tabs relative mb-40">
+                    <div className="widget-content-tab">
+                        <div className="widget-content-inner">
+                            <motion.div
+                                className="featured-item style-bottom"
+                                variants={containerVariants}
+                                initial="hidden"
+                                animate="visible"
+                            >
+                                <Swiper
+                                    modules={[Navigation, Pagination]}
+                                    spaceBetween={26.7}
+                                    slidesPerView={1}
+                                    navigation
+                                    pagination={{ clickable: true }}
+                                    observer={true}
+                                    observeParents={true}
+                                    breakpoints={{
+                                        768: { slidesPerView: 2 },
+                                        1300: { slidesPerView: 3 },
+                                        1500: { slidesPerView: 4 }
+                                    }}
+                                    className="featured pt-10 swiper-container carousel1"
+                                >
+                                    {nfts.map(nft => (
+                                        <SwiperSlide className="relative z-99999" key={nft._id}>
+                                            <Link to={`/nft/${nft._id}`}>
+                                                <motion.div className="tf-card-box style-4" variants={itemVariants}>
+                                                    <div className="author flex items-center">
+                                                        <div className="avatar">
+                                                            <img src={nft.author.profilePicture || profilePic} alt={nft.author.username} />
                                                         </div>
-                                                        <div className="card-media">
-                                                            <img src={nft.imageUrl} alt={nft.name} />
-                                                            <span className="wishlist-button icon-heart" />
-                                                        </div>
-                                                        <h5 className="name"><a href="nft-detail-2.html">{nft.name}</a></h5>
-                                                        <div className="meta-info flex items-center justify-between">
-                                                            <div>
-                                                                <span className="text-bid">Current Bid</span>
-                                                                <h6 className="price gem"><i className="icon-gem" />{nft.price}</h6>
-                                                            </div>
-                                                            <div className="button-place-bid">
-                                                                <button
-                                                                    data-toggle="modal"
-                                                                    data-target="#popup_bid"
-                                                                    className="tf-button"
-                                                                    onClick={(event) => {
-                                                                        event.preventDefault(); 
-                                                                        placeBid(nft._id);
-                                                                    }}>
-                                                                    <span>Place Bid</span>
-                                                                </button>
-                                                            </div>
+                                                        <div className="info">
+                                                            <span>Created by:</span>
+                                                            <h6><a href={`author-2.html`}>{nft.author.username}</a></h6>
                                                         </div>
                                                     </div>
-                                                </Link>
-                                            </SwiperSlide>
-                                        ))}
-
-
-                                    </Swiper>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                    <div className="heading-section">
-                        <h2 className="tf-title style-1 pb-30">Recently added</h2>
-                    </div>
-                    <div className="widget-tabs relative">
-                        <div className="widget-content-tab">
-                            <div className="widget-content-inner">
-                                <div className="wrap-box-card">
-                                    {latestNfts.map(nft => (
-                                        <div className="col-item" key={nft._id}>
-                                           <Link to={`/nft/${nft._id}`}>
-                                           <div className="tf-card-box style-1">
-                                                <div className="card-media">
-
-                                                    <img src={nft.imageUrl || profilePic} alt={nft.name} />
-                                                    <span className="wishlist-button icon-heart" />
-                                                    <div className="button-place-bid">
-                                                        <button onClick={() => placeBid(nft._id)} data-toggle="modal" data-target="#popup_bid" className="tf-button"><span>Place Bid</span></button>
+                                                    <div className="card-media">
+                                                        <img src={nft.imageUrl} alt={nft.name} />
+                                                        <span className="wishlist-button icon-heart" />
                                                     </div>
-                                                </div>
-                                                <h5 className="name">{nft.name}</h5>
-                                                <div className="author flex items-center">
-                                                    <div className="avatar">
-                                                        <img src={nft.author.profilePicture} alt={nft.author.username} />
+                                                    <h5 className="name"><a href="nft-detail-2.html">{nft.name}</a></h5>
+                                                    <div className="meta-info flex items-center justify-between">
+                                                        <div>
+                                                            <span className="text-bid">Current Bid</span>
+                                                            <h6 className="price gem"><i className="icon-gem" />{nft.price}</h6>
+                                                        </div>
+                                                        <div className="button-place-bid">
+                                                            <button
+                                                                data-toggle="modal"
+                                                                data-target="#popup_bid"
+                                                                className="tf-button"
+                                                                onClick={(event) => {
+                                                                    event.preventDefault(); 
+                                                                    placeBid(nft._id);
+                                                                }}>
+                                                                <span>Place Bid</span>
+                                                            </button>
+                                                        </div>
                                                     </div>
-                                                    <div className="info">
-                                                        <span>Created by:</span>
-                                                        <h6>{nft.author.username}</h6>
-                                                    </div>
-                                                </div>
-                                                <div className="divider" />
-                                                <div className="meta-info flex items-center justify-between">
-                                                    <span className="text-bid">Current Bid</span>
-                                                    <h6 className="price gem"><i className="icon-gem" />{nft.price}</h6>
-                                                </div>
-                                            </div>
-                                           </Link>
-                                        </div>
+                                                </motion.div>
+                                            </Link>
+                                        </SwiperSlide>
                                     ))}
-                                </div>
-                            </div>
+                                </Swiper>
+                            </motion.div>
                         </div>
                     </div>
-
                 </div>
 
+                <div className="heading-section">
+                    <h2 className="tf-title style-1 pb-30">Recently added</h2>
+                </div>
+                <div className="widget-tabs relative">
+                    <div className="widget-content-tab">
+                        <div className="widget-content-inner">
+                            <div className="wrap-box-card">
+                                <motion.div
+                                    className="row"
+                                    variants={containerVariants}
+                                    initial="hidden"
+                                    animate="visible"
+                                >
+                                    {latestNfts.map(nft => (
+                                        <motion.div className="col-item" key={nft._id} variants={itemVariants}>
+                                            <Link to={`/nft/${nft._id}`}>
+                                                <div className="tf-card-box style-1">
+                                                    <div className="card-media">
+                                                        <img src={nft.imageUrl || profilePic} alt={nft.name} />
+                                                        <span className="wishlist-button icon-heart" />
+                                                        <div className="button-place-bid">
+                                                            <button onClick={() => placeBid(nft._id)} data-toggle="modal" data-target="#popup_bid" className="tf-button"><span>Place Bid</span></button>
+                                                        </div>
+                                                    </div>
+                                                    <h5 className="name">{nft.name}</h5>
+                                                    <div className="author flex items-center">
+                                                        <div className="avatar">
+                                                            <img src={nft.author.profilePicture} alt={nft.author.username} />
+                                                        </div>
+                                                        <div className="info">
+                                                            <span>Created by:</span>
+                                                            <h6>{nft.author.username}</h6>
+                                                        </div>
+                                                    </div>
+                                                    <div className="divider" />
+                                                    <div className="meta-info flex items-center justify-between">
+                                                        <span className="text-bid">Current Bid</span>
+                                                        <h6 className="price gem"><i className="icon-gem" />{nft.price}</h6>
+                                                    </div>
+                                                </div>
+                                            </Link>
+                                        </motion.div>
+                                    ))}
+                                </motion.div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+    </div>
+);
 
-    );
 }
