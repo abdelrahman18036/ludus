4fa218d61953ea39414a8077ba7bcb66cd243fd2
Abdelrahman Abdelnasser Gamal <2021170298@cis.asu.edu.eg>
2024-05-05 12:08:10 +0300

    Update API.js

--- front-end/src/Components/Auth/API.js
+++ front-end/src/Components/Auth/API.js
@@ -1,2 +1,2 @@
 // export const baseURL = "http://localhost:5000";
-export const baseURL = "https://gaming-ecommerce-48xx.vercel.app";
+export const baseURL = "https://ludus-backend-8egb.onrender.com";
